7e53e12116e3379302c17e35d8604c0cff40c767
nguyenlam <sweetloveinyourheart@gmail.com>
2024-07-01 21:51:53 +0700

    Add database schema

---
+++ board_service/internal/schemas/board.go
@@ -0,0 +1,12 @@
+package schemas
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type BoardSchema struct {
+	ID          primitive.ObjectID `bson:"_id"`
+	Title       string             `bson:"title"`
+	Description string             `bson:"description"`
+	CreatedBy   string             `bson:"created_by"`
+	CreatedAt   int64              `bson:"created_at"`
+	UpdatedAt   int64              `bson:"updated_at"`
+}
